66728740176877c2e541946122f9e55015d915d1
markgietl <markgietl4@gmail.com>
2022-03-17 21:26:06 +0200

    First commit

---
+++ enhanced-if-statements-challenge/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace enhancedIfStatements
+{
+    internal class program
+    {
+        public static void Main()
+        {
+            checkWeather();
+        }
+
+        public static void checkWeather()
+        {
+            int temp;
+            string weather;
+            bool success = false;
+            Console.WriteLine("Enter the current temperature.");
+
+            while (!success)
+            {
+                string? tempInput = Console.ReadLine();
+
+                if (success = int.TryParse(tempInput, out temp))
+                {
+                    weather = temp <= 15 ? "Too cold" : temp >= 16 && temp <= 28 ? "Just right" : "Too hot";
+                    Console.WriteLine(weather);
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid input, please enter a number.");
+                }
+            }
+        }
+    }
+}
